76e516517f35c3f7f13a108587b7b1ff0fb61dcf
hyoinkang <hyoin1704@gmail.com>
2024-11-08 22:23:02 +0900

    chore : DB 연결 기본 설정

---
+++ app/config.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+from functools import lru_cache
+
+load_dotenv()
+
+class Settings():
+    DB_USER = os.environ.get("DB_USER")
+    DB_URL = os.environ.get("DB_URL")
+    DB_PASS = os.environ.get("DB_PASS")
+    DB_NAME = os.environ.get("DB_NAME")
+    DB_PORT = int(os.environ.get("DB_PORT", 3306))
+
+@lru_cache
+def get_settings():
+    return Settings()
+
+settings = get_settings()
--- app/main.py
+++ app/main.py
@@ -1,7 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from contextlib import asynccontextmanager
+from sqlalchemy.orm import Session
+
+from app.database import get_db, Base
+from app.models.models import Example
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    from app import models
+    from app.database import engine
+
+    models.Base.metadata.create_all(bind=engine)
+
+    yield
+
+    pass
 
 app = FastAPI()
 
 @app.get("/health")
-def healthCheck():
+def healthCheck(db: Session = Depends(get_db)):
+    new_entry = Example(url=str("health"))
+    db.add(new_entry)
+    db.commit()
+    db.refresh(new_entry)
     return "I'm healthy!"
