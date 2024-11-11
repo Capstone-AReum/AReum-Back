227e41c47bf2c8a7d46f223553cade157d263b39
hyoinkang <hyoin1704@gmail.com>
2024-11-12 02:53:41 +0900

    feat : 로컬에서 s3 연결

--- app/main.py
+++ app/main.py
@@ -5,6 +5,9 @@ from sqlalchemy.orm import Session
 from app.database import get_db
 from app.models.models import Example
 
+from fastapi import UploadFile, File, HTTPException
+from app.s3_utils import upload_to_s3
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     from app.models import models
@@ -20,8 +23,16 @@ app = FastAPI(lifespan=lifespan)
 
 @app.get("/health")
 def healthCheck(db: Session = Depends(get_db)):
-    new_entry = Example(url=str("health"))
-    db.add(new_entry)
-    db.commit()
-    db.refresh(new_entry)
+#    new_entry = Example(url=str("health"))
+#    db.add(new_entry)
+#    db.commit()
+#    db.refresh(new_entry)
     return "I'm healthy!"
+
+@app.post("/upload")
+async def upload_file(file: UploadFile = File(...)) -> dict:
+    try:
+        file_url=upload_to_s3(file)
+        return {"file_url": file_url}
+    except RuntimeError as e:
+        raise HTTPException(status_code=500, detail=str(e))
