90debf143d33d2536f1f885f15da1efa1f627262
hyoinkang <hyoin1704@gmail.com>
2024-12-02 22:29:32 +0900

    refactor : s3 업로드 확장자 관련 리팩토링

--- app/utils/s3_utils.py
+++ app/utils/s3_utils.py
@@ -1,4 +1,5 @@
 import os
+import mimetypes
 from botocore.exceptions import BotoCoreError, ClientError
 from ..config import s3_client
 
@@ -7,11 +8,17 @@ def upload_to_s3(file, path) -> None:
         bucket_name=os.getenv("S3_BUCKET")
         region=os.getenv("AWS_REGION")
         s3_path=f"{path}/{file.filename}"
+
+        #파일의 ContentType 동적으로 설정
+        content_type, _=mimetypes.guess_type(file.filename)
+        if not content_type:
+            content_type="application/octet-stream"
+
         s3_client.upload_fileobj(
             file.file,
             bucket_name,
             s3_path,
-            ExtraArgs={"ContentType": "image/jpeg"},
+            ExtraArgs={"ContentType": content_type},
         )
         file_url = f"https://{bucket_name}.s3.{region}.amazonaws.com/{s3_path}"
         return file_url
